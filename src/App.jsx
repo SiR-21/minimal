2c32ea384407cd80e5b2bc565fb0472a5126e9c7
Daniel <daniel.noshadha21@gmail.com>
2025-09-02 23:00:26 +0200

    Added insight

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const App = () => {
   const [time, setTime] = useState(new Date());
@@ -38,6 +39,7 @@ const App = () => {
 
   return (
     <div className="app-fade-in">
+      <SpeedInsights/>
       <Header time={time} theme={theme} />
 
       <div className="top-left-corner">
